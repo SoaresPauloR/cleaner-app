5f54bacc3c37f539813387667965b9c75b067997
Paulo Ricardo Soares <ftp.soares@gmail.com>
2024-07-18 22:12:37 +0100

    Create Footer and Header

--- src/components/Footer.tsx
+++ src/components/Footer.tsx
@@ -1,7 +1,11 @@
 import React from 'react';
 
 function Footer() {
-  return <footer className="w-full h-16"></footer>;
+  return (
+    <footer className="w-full h-10 flex items-center justify-center text-gray-600">
+      <p>All Rights Reserved</p>
+    </footer>
+  );
 }
 
 export default Footer;
--- src/components/Header.tsx
+++ src/components/Header.tsx
@@ -5,7 +5,14 @@
 import React from 'react';
 
 const Header = () => {
-  return <header className="w-full h-24"></header>;
+  return (
+    <header className="w-full h-24 flex items-center ps-10 pe-10">
+      <div>
+        <h3 className="text-lg">Hi, Mr Paulo. It&lsquo;s a good day </h3>
+        <h4 className="text-sm text-gray-700">Today is Wednesday June 15</h4>
+      </div>
+    </header>
+  );
 };
 
 export default Header;
